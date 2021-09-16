fc7d2b51b077a34422e9efafdbfbfc31c2a31dd1
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-16 13:41:53 +0300

    Добавьте файлы проекта.

---
+++ Static Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Static_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            description();
+        }
+
+
+
+        public static void description()
+        {
+
+            string username = "carl";
+            string role = "admin";
+
+            {
+
+                Console.WriteLine($"User name is: {username} \nUser role is: {role}");
+            }
+
+
+        }
+    }
+
+
+
+
+
+
+    }
